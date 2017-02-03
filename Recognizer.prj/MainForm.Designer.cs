7d71b1426e0fca23b277cc0175965c32847336b0
Andrey Okomin <nomer12f@yandex.ru>
2017-02-03 15:54:09 +0300

    Added connection test and camera test

---
+++ Recognizer.prj/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Recognizer.prj
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._btnTestOpenCV = new System.Windows.Forms.Button();
+			this._btnTestCamera = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// _btnTestOpenCV
+			// 
+			this._btnTestOpenCV.Location = new System.Drawing.Point(140, 12);
+			this._btnTestOpenCV.Name = "_btnTestOpenCV";
+			this._btnTestOpenCV.Size = new System.Drawing.Size(132, 23);
+			this._btnTestOpenCV.TabIndex = 0;
+			this._btnTestOpenCV.Text = "Test OpenCV";
+			this._btnTestOpenCV.UseVisualStyleBackColor = true;
+			this._btnTestOpenCV.Click += new System.EventHandler(this.OnButtonTestOpenCV_Click);
+			// 
+			// _btnTestCamera
+			// 
+			this._btnTestCamera.Location = new System.Drawing.Point(140, 41);
+			this._btnTestCamera.Name = "_btnTestCamera";
+			this._btnTestCamera.Size = new System.Drawing.Size(132, 23);
+			this._btnTestCamera.TabIndex = 1;
+			this._btnTestCamera.Text = "Test Camera";
+			this._btnTestCamera.UseVisualStyleBackColor = true;
+			this._btnTestCamera.Click += new System.EventHandler(this.OnButtonTestCamera_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this._btnTestCamera);
+			this.Controls.Add(this._btnTestOpenCV);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "MainForm";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button _btnTestOpenCV;
+		private System.Windows.Forms.Button _btnTestCamera;
+	}
+}
+
---
+++ Recognizer.prj/MainForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace Recognizer.prj
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void OnButtonTestOpenCV_Click(object sender, EventArgs e)
+		{
+			using(IplImage image = new IplImage(256, 256, BitDepth.U8, 1))
+			{
+				image.Zero();
+				for(int x = 0; x < image.Width; x++)
+				{
+					for(int y = 0; y < image.Height; y++)
+					{
+						int offset = y * image.WidthStep + x;
+						byte value = (byte)(x + y);
+						Marshal.WriteByte(image.ImageData, offset, value);
+					}
+				}
+				using(CvWindow window = new CvWindow("Success!", WindowMode.AutoSize, image))
+				{
+					CvWindow.WaitKey();
+				}
+			}
+		}
+
+		private void OnButtonTestCamera_Click(object sender, EventArgs e)
+		{
+			IplImage frame = null;
+			IplImage src = null;
+			CvCapture capture = null;
+			
+			//CvWindow windowCapture = new CvWindow("Camera capture");
+
+			using(CvWindow windowCapture = new CvWindow("Camera capture", WindowMode.KeepRatio))
+			{
+				try
+				{
+					capture = new CvCapture(0);
+				}
+				catch
+				{
+					Debug.WriteLine("Error: can't open camera.");
+				}
+
+				if(capture != null)
+				{
+
+					double width = capture.GetCaptureProperty(CaptureProperty.FrameWidth);
+					double height = capture.GetCaptureProperty(CaptureProperty.FrameHeight);
+
+					Debug.WriteLine("Width: " + width);
+					Debug.WriteLine("Height: " + height);
+
+					int counter = 0;
+					while(true)
+					{
+						capture.GrabFrame();
+						frame = capture.RetrieveFrame();
+
+						windowCapture.ShowImage(frame);
+
+						int key = CvWindow.WaitKey(33);
+						if(key == 27) // ESC 
+						{
+							windowCapture.Close();
+							break;
+
+						}
+						else if(key == 13) //Enter 
+						{
+							frame.SaveImage($"Image{ counter}.jpg");
+							counter++;
+						}
+					}
+				}
+				else
+				{
+					Debug.WriteLine("Error: can't open camera.");
+				}
+				Debug.WriteLine("End");
+			}				
+		}
+	}
+}
